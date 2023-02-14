56a4e1d13b9e34e59aa4f2aa61ea28e5fee89365
abraao69 <fullstack695@gmail.com>
2023-02-14 14:36:19 -0300

    Curso-Desenvolvedor-Web-Completo

---
+++ 13-PHP_AJAX/unidade_11/retornar_produtos.php
@@ -0,0 +1,25 @@
+<?php 
+    $callback = isset($_GET['callback']) ?  $_GET['callback'] : false;
+    $conecta = mysqli_connect("localhost","root","","andes");
+    mysqli_set_charset($conecta, 'utf8');
+
+    if(isset($_GET['categoriaID'])) {
+        $catID = $_GET['categoriaID'];
+    } else {
+        $catID = 1;
+    }
+
+    $selecao  = "SELECT produtoID, nomeproduto FROM produtos ";
+    $selecao .= "WHERE categoriaID = {$catID}";
+    $produtos = mysqli_query($conecta,$selecao);
+
+    $retorno = array();
+    while($linha = mysqli_fetch_object($produtos)) {
+        $retorno[] = $linha;
+    } 	
+
+    echo json_encode($retorno);
+    
+    // fechar conecta
+    mysqli_close($conecta);
+?>
